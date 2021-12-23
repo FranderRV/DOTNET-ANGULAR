6b77df2e21ee11a533dffa141f3b89d40084a573
Frander <61984867+FranderRV@users.noreply.github.com>
2021-12-23 20:41:01 +0100

    CRUD Completado

--- FRONTEND/src/app/services/tarjeta-service.service.ts
+++ FRONTEND/src/app/services/tarjeta-service.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs'; 
 import { Tarjeta } from '../models/Tarjeta';
 
 @Injectable({
@@ -9,11 +9,35 @@ import { Tarjeta } from '../models/Tarjeta';
 export class TarjetaService {
   base = 'https://localhost:7081/'
   rute = 'api/Tarjetas/'
-  constructor(private hhtp:HttpClient) { 
+  tarjetas:any[] = [];
+  private actualizarForm = new BehaviorSubject<Tarjeta>({} as any)
+  constructor(private http:HttpClient) { 
 
   }
 
   guardarTarjeta(tarjeta:Tarjeta):Observable<Tarjeta>{
-   return this.hhtp.post<Tarjeta>(this.base+this.rute,tarjeta)
+   return this.http.post<Tarjeta>(this.base+this.rute,tarjeta)
+  }
+
+  obtenerTarjetas(){
+    this.http.get(this.base+this.rute).toPromise()
+    .then(tarjetas=>{
+      this.tarjetas = tarjetas as Tarjeta[]   
+    })
+  }
+
+  eliminarTarjeta(id:Number):Observable<Tarjeta>{
+    return this.http.delete<Tarjeta>(this.base+this.rute+id)
+  }
+
+  actualizarTarjeta(id:number,tarjeta: Tarjeta):Observable<Tarjeta>{
+    return this.http.put<Tarjeta>(this.base+this.rute+id,tarjeta);
+  }
+
+  actualizarTarjetaFormulario(tarjeta: Tarjeta){
+    this.actualizarForm.next(tarjeta)
+  }
+  obtenerTarjetaFormulario():Observable<Tarjeta>{
+    return this.actualizarForm.asObservable()
   }
 }
